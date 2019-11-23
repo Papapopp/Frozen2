d04aac937ef525934f76eb1350960c65389eef17
Zhuyi Liu <32209806+liuzhuyi1999@users.noreply.github.com>
2019-11-22 20:00:59 -0700

    MovingPlatformDemo

---
+++ Frozen2v12/Assets/PFDemo/MoveLava.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveLava : MonoBehaviour
+{
+    public float speed;
+    public bool direction;
+    public float maxtimer;
+    private float timer;
+    public void Start()
+    {
+        if (direction == false)
+        {
+            timer = maxtimer;
+        }
+        else timer = 0;
+    }
+    public void Update()
+    {
+        if (direction == true)
+        {
+            timer += Time.deltaTime;
+        }
+        else timer -= Time.deltaTime;
+        if (timer<=0)
+        {
+            direction = true;
+        }
+        if (timer>=maxtimer)
+        {
+            direction = false;
+        }
+        if (direction == true)
+        {
+            transform.position=transform.position + new Vector3(0, speed,0);
+        } else transform.position = transform.position - new Vector3(0, speed, 0);
+    }
+}
---
+++ Frozen2v12/Assets/PFDemo/MovePlatform.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePlatform : MonoBehaviour
+{
+    public float speed;
+    public bool direction;
+    public float maxtimer;
+    private float timer;
+    public void Start()
+    {
+        if (direction == false)
+        {
+            timer = maxtimer;
+        }
+        else timer = 0;
+    }
+    public void Update()
+    {
+        if (direction == true)
+        {
+            timer += Time.deltaTime;
+        }
+        else timer -= Time.deltaTime;
+        if (timer<=0)
+        {
+            direction = true;
+        }
+        if (timer>=maxtimer)
+        {
+            direction = false;
+        }
+        if (direction == true)
+        {
+            transform.position=transform.position + new Vector3(speed, 0,0);
+        } else transform.position = transform.position - new Vector3(speed, 0, 0);
+    }
+}
