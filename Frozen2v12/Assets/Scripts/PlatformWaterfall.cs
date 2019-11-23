3bc1c090160129743c097e43eaabd459bfa66224
Wyatt Sanders <u1139272@ad.utah.edu>
2019-11-22 22:02:09 -0700

    falling and waterfalling platforms

---
+++ Frozen2v12/Assets/Scripts/PlatformWaterfall.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformWaterfall : MonoBehaviour
+{
+    
+    public float speed;
+    public bool isFrozen;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+    }
+
+}
