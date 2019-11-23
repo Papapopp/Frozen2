f61b4b77f52f8b396f533d8fe1c90cf7c70bc378
Adam Draheim <u1058068@ad.utah.edu>
2019-11-22 18:00:01 -0700

    Made store manager and purchase prefs

--- Frozen2Project/Assets/Scripts/Mt_scripts/Store_Manager.cs
+++ Frozen2Project/Assets/Scripts/Mt_scripts/Store_Manager.cs
@@ -2,8 +2,36 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/**
+ * Adam Draheim
+ * Simple Unity Microtransaction store manager
+ * */
 public class Store_Manager : MonoBehaviour
 {
+
+    public enum purchase
+    {
+        character,
+        level
+    }
+
+    //Struct for holding buyables
+    [System.Serializable]
+    public struct Buyable{
+        public string name;
+        public Sprite storeImage;
+        public int cost;
+        public int buyID;
+        public purchase purchase_type;
+    };
+
+    public Buyable[] store;
+
+    private void Awake()
+    {
+        
+    }
+
     // Start is called before the first frame update
     void Start()
     {
